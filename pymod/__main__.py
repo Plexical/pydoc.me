f396e790647dd645a95c134f39a980908c367d1b
Jacob Oscarson <jacob@plexical.com>
2016-05-18 12:15:47 +0200

    Serve static files from the `static/` directory when developing

--- pymod/__main__.py
+++ pymod/__main__.py
@@ -1,5 +1,10 @@
 from routes import app
 
 if __name__ == "__main__":
+    import os
+    from werkzeug import SharedDataMiddleware
+
+    static_base = os.path.join(os.getcwd(), 'static')
+    app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {'/static': static_base})
     app.debug = True
     app.run(host='0.0.0.0')
