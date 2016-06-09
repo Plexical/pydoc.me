ca8aef1a6b83339400f3bc7b80ea41972a2f460f
Jacob Oscarson <jacob@plexical.com>
2016-06-09 11:17:36 +0200

    Moved module indexing to own module

--- grabtools.py
+++ grabtools.py
@@ -27,3 +27,40 @@ def anchors(name, url):
     class_ = {'class': 'headerlink'}
     As = (a.attrs['href'].split('#')[1] for a in dom.findAll('a', class_))
     return [a for a in As if a.startswith(name)]
+
+def index_modules():
+    "Loads all shortcuts from cache or downloads from docs.python.org"
+
+    cache = './.modules'
+
+    if os.path.exists(cache):
+        with open(cache, 'rb') as fp:
+            return load(fp)
+
+    else:
+        master = {}
+
+        for m3 in modsof('3'):
+            mod_url = py3b+'/library/{}.html'.format(m3)
+            master[m3] = { '3': { 'M': mod_url } }
+            print('Anchors/{} for {}..'.format('3', m3))
+            for anchor in anchors(m3, master[m3]['3']['M']):
+                master[m3]['3'] = dict(
+                    master[m3]['3'],
+                    **{ anchor:
+                        '{}/library/{}.html#{}'.format(py3b, m3, anchor) })
+
+        for m2 in modsof('2'):
+            mod_url = py2b+'/library/{}.html'.format(m2)
+            if m2 in master:
+                master[m2]['2'] = { 'M': mod_url }
+            else:
+                master[m2] = {'2': {'M': mod_url}}
+            print('Anchors/{} for {}..'.format('2', m2))
+            for anchor in anchors(m2, master[m2]['2']['M']):
+                master[m2]['2'] = dict(
+                    master[m2]['2'],
+                    **{ anchor: '{}/library/{}.html#{}'.format(py2b, m2, anchor) })
+        with open(cache, 'wb') as fp:
+            dump(master, fp)
+            return master
